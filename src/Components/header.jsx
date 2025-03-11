5fd64a6eee449eb9d296e2911bc03e4de938a327
Puppycoder88 <jaingarima1795@gmail.com>
2025-03-11 13:13:11 +0530

    header

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function header() {
     return (
-        <div className="flex bg-orange-400 h-36 items-center ">
-           <div className="text-4xl font-bold flex-1 ml-10 mt-4">
+        <div className="flex bg-orange-400 h-36 items-center  ">
+           <div className="text-4xl font-bold flex-1 ml-10 my-4">
               <h1 >GEMBA</h1>
             </div> 
-            <div className="flex-1 text-white text-3xl mt-4 font-bold">
+            <div className="flex-1 text-white text-3xl my-4 font-bold">
               <h2>Invoice Tracking System</h2>
             </div>
         </div>
